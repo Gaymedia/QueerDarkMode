216b1217c3beee58a8858340e6215d3b82ecda37
Gaymedia <gaygit@traber-info.de>
2019-11-12 19:11:43 +0100

    Added all-in-one support for all browsers
    Made this plugin version compatible with Chrome, Firefox and Opera.

--- js/modify_dark.js
+++ js/modify_dark.js
@@ -1,3 +1,6 @@
+if (typeof(chrome) == "undefined") {
+    var chrome = browser;
+}
 var darkCssNode = document.createElement('link');
 darkCssNode.rel = 'stylesheet';
 darkCssNode.href = chrome.runtime.getURL('css/dark.css');
